dca3ed2a3e44e55dc160c132f18f327934c79410
Marge13333 <113912686+Marge13333@users.noreply.github.com>
2023-05-08 22:35:42 +0400

    Fixin second section

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+////////////Slider
+
 function Slider() {
 	const slide = document.querySelectorAll(".header-bg");
 	
@@ -30,3 +33,107 @@ function Slider() {
 
 }
 Slider();
+
+
+/////////////Skil Lines
+
+function HtmlCssLine(){
+	const linesFirst = document.querySelectorAll('.lines-80')
+
+window.addEventListener('scroll', checkBox)
+
+checkBox()
+
+
+function checkBox(){
+	const triggerBottom = window.innerHeight ;
+
+	linesFirst.forEach((box) =>{
+		const boxTop = box.getBoundingClientRect().top;
+
+
+		if(boxTop < triggerBottom){
+			box.classList.add('actionHtmlCss')
+		}else{
+			box.classList.remove("actionHtmlCss")
+		}
+		
+	})
+}
+}
+HtmlCssLine()
+
+function ReactJsLine(){
+	const linesSecond = document.querySelectorAll('.lines-50')
+
+window.addEventListener('scroll', checkBox)
+
+checkBox()
+
+
+function checkBox(){
+	const triggerBottom = window.innerHeight ;
+
+	linesSecond.forEach((box) =>{
+		const boxTop = box.getBoundingClientRect().top;
+
+
+		if(boxTop < triggerBottom){
+			box.classList.add('actionReactJs')
+			
+		}else{
+			box.classList.remove("actionReactJs")
+		}
+		
+	})
+}
+}
+ReactJsLine()
+
+function HtmlCssCount(){
+	let valueDisplays = document.querySelectorAll(".num")
+	let interval = 2000;
+
+	valueDisplays.forEach((valueDisplay) =>{
+		let startValue = 0;
+		let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+		let duration = Math.floor(interval/endValue);
+		let counter = setInterval(function(){
+			startValue += 1;
+			valueDisplay.textContent= startValue;
+			if (startValue === endValue){
+				clearInterval(counter);
+			}
+		},duration)
+	}
+	)
+}
+
+
+///////// Second Section animation
+
+function SecondSectionTransition(){
+	const MuchInfo = document.querySelectorAll('.info-wrapper')
+
+window.addEventListener('scroll', checkBox)
+
+checkBox()
+
+
+function checkBox(){
+	const triggerBottom = window.innerHeight ;
+
+	MuchInfo.forEach((box) =>{
+		const boxTop = box.getBoundingClientRect().top;
+
+
+		if(boxTop < triggerBottom){
+			box.classList.add('action')
+			HtmlCssCount()
+		}
+		
+	})
+}
+}SecondSectionTransition()
+
+
